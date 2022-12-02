438e20ef9668df6610474c5d4430a53337489e95
ishacheema <60581256+ishacheema@users.noreply.github.com>
2022-12-02 01:39:06 -0500

    Fixes add buyer and seller endpoints

--- e-commerce-webapp/src/requests.js
+++ e-commerce-webapp/src/requests.js
@@ -45,16 +45,16 @@ async function add_seller(user) {
       "Content-Type": "application/x-www-form-urlencoded",
     },
     data: {
-      first_name: user.first_name_ref.current.value,
-      last_name: user.first_name_ref.last_name,
-      email: user.first_name_ref.email,
-      username: user.first_name_ref.username,
-      password: user.first_name_ref.password,
-      phone_number: user.first_name_ref.phoneNumber,
-      address: user.addrss_ref.address,
-      opt_in: user.opt_in_ref.opt_in,
-      store_name: user.store_name_ref.store_name,
-      store_id: user.store_id_ref.store_id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      username: user.username,
+      password: user.password,
+      phone_number: user.phone_number,
+      address: user.address,
+      opt_in: user.opt_in,
+      store_name: user.store_name,
+      store_id: user.store_id,
     },
   })
     .then((response) => {
@@ -67,21 +67,8 @@ async function add_seller(user) {
 }
 
 //ADD BUYER
-async function add_buyer(first_name, last_name,email,username, password, phone_number, address, opt_in, store_name, store_id) {
+async function add_buyer(user) {
   console.log("Attempting to add new buyer....");
-  var fn = first_name.first_name_ref.current.value;
-  var ln = last_name.last_name_ref.current.value;
-  var e = email.username_ref.current.value;
-  var u = username.username_ref.current.value;
-  var p = password.password_ref.current.value;
-  var pn = phone_number.phone_number_ref.current.value
-  var a = address.addrss_ref.current.value;
-  var o = opt_in.opt_in_ref.current.value
-  var sN = store_name.store_name_ref.current.value;
-  var sID = store_id.store_id_ref.current.value
-
-
-
   return await axios({
     method: "post",
     url: url + "/auth/addBuyer",
@@ -89,16 +76,16 @@ async function add_buyer(first_name, last_name,email,username, password, phone_n
       "Content-Type": "application/x-www-form-urlencoded",
     },
     data: {
-      first_name: fn,
-      last_name: ln,
-      email: e,
-      username: u,
-      password: p,
-      phone_number: pn,
-      address: a,
-      opt_in: o,
-      store_name: sN,
-      store_id: sID,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      username: user.username,
+      password: user.password,
+      phone_number: user.phone_number,
+      address: user.address,
+      opt_in: user.opt_in,
+      store_name: user.store_name,
+      store_id: user.store_id,
     },
   })
     .then((response) => {
